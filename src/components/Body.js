4eb030b4bafa9cd904af1b59a8e5105efced5527
anuraglahon16 <anuraglahondp@gmail.com>
2024-10-06 04:57:17 -0700

    filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import Reslist from "../utils/mockData";
+import { useState } from "react";
+
 const Body =() =>{
+    const[listofRestaurants,setListofRestaurents] = useState(Reslist);
     return(
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn"
+                onClick={()=>{
+                    const filteredlist = listofRestaurants.filter(
+                        (res)=>res.info.avgRating>4.2
+                    );
+                    setListofRestaurents(filteredlist);
+                  
+                }}
+                >Top Rated Restaurent</button>
+                </div>
             <div className="res-container">
             {
-                Reslist.map((restaurent) => (<RestaurantCard key = {restaurent.info.id} resData = {restaurent}/>
+                listofRestaurants.map((restaurent) => (<RestaurantCard key = {restaurent.info.id} resData = {restaurent}/>
             )
         )
             }
